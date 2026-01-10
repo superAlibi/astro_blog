92278574b1a5e4c84753dedbfed2e23353497045
alibi <alibi_jia@outlook.com>
2026-01-10 17:44:45 +0800

    更新网站标题和描述为中文，修改日期格式为中文，更新项目管理文章的发布日期，添加robots.txt支持。

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '云逸尘的网络日志';
+export const SITE_DESCRIPTION = '一个前端牛马的网络日志';
