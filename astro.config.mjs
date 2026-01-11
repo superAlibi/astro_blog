5fa341ecbf1af2f6324b176c3515a2ea60779c15
alibi <alibi_jia@outlook.com>
2026-01-11 17:56:27 +0800

    网站装修

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,39 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 import astroMermaid from 'astro-mermaid';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luchador.dev',
-  integrations: [mdx(), sitemap(), astroMermaid()],
+  markdown: {
+
+    shikiConfig: {
+      // theme: 'dracula',
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+    },
+
+  },
+  integrations: [mdx(), sitemap({
+    filter: (page) => {
+      return !page.includes('/test')
+    },
+  }), astroMermaid()],
   adapter: cloudflare({
     imageService: 'compile',
   }),
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
   },
 });
